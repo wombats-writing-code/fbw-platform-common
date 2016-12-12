15e93906934f7021f6e149efbc95ce737a667666
cjshaw <cjshaw@mit.edu>
2016-12-12 09:35:37 -0500

    add utils, selectors, and consts for instructor app

--- selectors/index.js
+++ selectors/index.js
@@ -1,6 +1,8 @@
 
 import _ from 'lodash'
 
+import { BANK_TO_DOMAIN, DOMAIN_TO_LIBRARY, BANK_TO_LIBRARY } from './utilities'
+
 let moment = require('moment');
 require('moment-timezone');
 
@@ -105,4 +107,36 @@ export function getTargetQuestions (state) {
     questions = routeQuestions[targetKey(state.mission.currentTarget)]
   }
   return questions
+}
+
+export function findBankDomain (bankId, enrolledBanks) {
+  // handles both simple login (hardcoded bankIds) and D2L-linked banks
+  if (_.keys(BANK_TO_DOMAIN).indexOf(bankId) >= 0) {
+    return BANK_TO_DOMAIN[bankId]
+  } else {
+    console.log('bankId', bankId, 'enrolledBanks', enrolledBanks)
+    let department = _.find(enrolledBanks, {id: bankId}).department.toLowerCase()
+    console.log('department', department)
+    switch (department) {
+      case 'accounting':
+      case 'acc':
+        return 'accounting'
+
+      case 'algebra':
+      case 'alg':
+      case 'mat':
+      case 'math':
+      case 'collegealgebra':
+      case 'college_algebra':
+        return 'algebra'
+
+      default:
+        return 'accounting'
+    }
+  }
+}
+
+export function findBankLibrary (bankId, enrolledBanks) {
+  let department = findBankDomain(bankId, enrolledBanks)
+  return DOMAIN_TO_LIBRARY[department]
 }
--- utilities/index.js
+++ utilities/index.js
@@ -67,6 +67,19 @@ export function qbankToMoment(timeObject) {
   })
 }
 
+export function adjustedQBankToMoment(timeObject) {
+  // for mission times that were already adjusted in stores,
+  // and moment.js takes months as 1-12
+  return moment.utc({
+    years: timeObject.year,
+    months: timeObject.month + 1,
+    days: timeObject.day,
+    hours: timeObject.hour,
+    minutes: timeObject.minute,
+    second: timeObject.second
+  })
+}
+
 export function convertPythonDateToJS (pythonTime) {
   return {
     year: pythonTime.year,
@@ -235,8 +248,14 @@ export function flush () {
   }
 }
 
+export const SCHOOL_TO_BANK = {"acc": "assessment.Bank%3A57279fc2e7dde08807231e61%40bazzim.MIT.EDU",
+                               "qcc": "assessment.Bank%3A57279fcee7dde08832f93420%40bazzim.MIT.EDU"}
+
 export const BANK_TO_DOMAIN = {"assessment.Bank%3A57d70ed471e482a74879349a%40bazzim.MIT.EDU": "accounting",
                                "assessment.Bank%3A576d6d3271e4828c441d721a%40bazzim.MIT.EDU": "algebra"}
+
+export const DOMAIN_TO_LIBRARY = {"accounting": "assessment.Bank%3A57279fbce7dde086c7fe20ff%40bazzim.MIT.EDU",
+                                  "algebra": "assessment.Bank%3A57279fb9e7dde086d01b93ef%40bazzim.MIT.EDU"}
 
 export const BANK_TO_LIBRARY = {"assessment.Bank%3A57d70ed471e482a74879349a%40bazzim.MIT.EDU": "assessment.Bank%3A57279fbce7dde086c7fe20ff%40bazzim.MIT.EDU",
                                 "assessment.Bank%3A576d6d3271e4828c441d721a%40bazzim.MIT.EDU": "assessment.Bank%3A57279fb9e7dde086d01b93ef%40bazzim.MIT.EDU"}
