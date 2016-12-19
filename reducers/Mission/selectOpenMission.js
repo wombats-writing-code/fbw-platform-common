1821face0eebff0606f79620a0ef7cfcf24fefaa
cjshaw <cjshaw@mit.edu>
2016-12-19 15:26:46 -0500

    change behavior of selectOpenMission to expect a bankId arg

--- reducers/Mission/selectOpenMission.js
+++ reducers/Mission/selectOpenMission.js
@@ -30,7 +30,7 @@ export function selectOpenMission (data) {
     dispatch(openMissionOptimistic(data.mission))
 
     let options = {
-      url: `${getDomain()}/middleman/banks/${data.mission.assignedBankIds[0]}/offereds/${data.mission.assessmentOfferedId}/takeMission`,
+      url: `${getDomain()}/middleman/banks/${data.bankId}/offereds/${data.mission.assessmentOfferedId}/takeMission`,
       headers: {
         'x-fbw-username': data.username
       }
