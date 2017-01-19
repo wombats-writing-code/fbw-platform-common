b5133f32d9d3e74927015181763520cc1e822474
cjshaw <cjshaw@mit.edu>
2017-01-19 16:41:55 -0500

    add more detail to new student login spec file

--- __tests__/new-student-login.unit-spec.js
+++ __tests__/new-student-login.unit-spec.js
@@ -6,7 +6,9 @@ import axios from 'axios'
 import {
   authenticateD2LStudent
 } from '../reducers/Login/authenticateD2L'
-import {LOGGED_IN, logInUser} from '../reducers/Login/logInUser'
+import {logInUser} from '../reducers/Login/logInUser'
+import {RECEIVE_MISSIONS, getMissions} from '../reducers/Mission/getMissions'
+import {RECEIVE_CREATE_TAKE_MISSION, selectOpenMission} from '../reducers/Mission/selectOpenMission'
 
 import thunk from 'redux-thunk'
 import configureMockStore from 'redux-mock-store'
@@ -28,6 +30,8 @@ const BASE_URL = 'https://fbw-web-backend.herokuapp.com'
 const UNIQUE_USERNAME = Math.floor(new Date().getTime()).toString()
 const FAKE_SCHOOL = "testing"
 const LOGGED_IN_USERNAME = `${UNIQUE_USERNAME}@${FAKE_SCHOOL}.edu`
+
+let privateBankId
 
 describe('student web app', () => {
   it('should not allow unauthorized students to make qbank calls', function (done) {
@@ -62,127 +66,88 @@ describe('student web app', () => {
     })
   })
 
+  // @luwenh -- help here, getting a 400 error about http vs. https when I run
+  // this block??
   // it('should create / retrieve the private bank when calling authenticateD2LStudent and create authz', done => {
   //   // You can now make middleman calls to getMission (which
   //   //   calculates the privateBankId for you)
-  //   let credentials = _.assign({}, require('../../../d2lcredentials'), {
+  //   let credentials = _.assign({}, require('../d2lcredentials'), {
   //     role: 'student'
   //   })
   //
-  //   const store = mockStore({})
+  //   const store = mockStore({
+  //     login: {
+  //       user: {
+  //         username: LOGGED_IN_USERNAME
+  //       },
+  //       isLoggedIn: true,
+  //       isVisitor: false
+  //     }
+  //   })
+  //
+  //   let expectedAction = {
+  //     type: RECEIVE_MISSIONS,
+  //
+  //   }
   //
   //   store.dispatch(authenticateD2LStudent(credentials))
-  //   .then( (data) => {
-  //     // data = {url, banks, username} but we shouldn't care what it is...
-  //     return chai.request(BASE_URL)
-  //     .get(`/middleman/banks/${MAT_BANK_ID}`)
-  //     .set('x-fbw-username', LOGGED_IN_USERNAME)
-  //     store.getActions().should.be.eql(expectedAction)
+  //   .then( () => {
+  //     // Here I want to check that I can getMissions for the MAT_BANK_ID
+  //     // assuming that the mocked student is enrolled in that course??
+  //   //   return store.dispatch(getMissions({
+  //   //     subjectBankId: MAT_BANK_ID,
+  //   //     username: LOGGED_IN_USERNAME
+  //   //   }))
+  //   // })
+  //   // .then( () => {
+  //     // privateBankId = state.bank.privateBankId -- need this for cleanup
+  //     console.log(store.getActions())
+  //     // store.getActions().should.be.eql(expectedAction)
+  //     // list of missions should be > 0
   //   })
   // })
-  // //
-  // // it('should be able to get a list of D2L shared missions', done => {
-  // //   // this verifies that the private bank was inserted into the hierarchy
-  // //   //    correctly
-  // //   let credentials = _.assign({}, require('../../../d2lcredentials'), {
-  // //     role: 'student'
-  // //   })
-  // //
-  // //   const expectedAction = {
-  // //     type: RECEIVE_AUTHENTICATE_D2L,
-  // //     credentials
-  // //   }
-  // //   const store = mockStore({})
-  // //
-  // //   store.dispatch(authenticateD2LStudent(credentials))
-  // //   .then( () => {
-  // //     store.getActions().should.be.eql(expectedAction)
-  // //   })
-  // // })
-  // //   it('should be able to get questions for a mission', done => {
-  // //     let credentials = _.assign({}, require('../../../d2lcredentials'), {
-  // //       role: 'student'
-  // //     })
-  // //
-  // //     const expectedAction = {
-  // //       type: RECEIVE_AUTHENTICATE_D2L,
-  // //       credentials
-  // //     }
-  // //     const store = mockStore({})
-  // //
-  // //     store.dispatch(authenticateD2LStudent(credentials))
-  // //     .then( () => {
-  // //       store.getActions().should.be.eql(expectedAction)
-  // //     })
-  // //   })
-  // //
-  // //   it('should be able to get submit responses for a mission', done => {
-  // //     let credentials = _.assign({}, require('../../../d2lcredentials'), {
-  // //       role: 'student'
-  // //     })
-  // //
-  // //     const expectedAction = {
-  // //       type: RECEIVE_AUTHENTICATE_D2L,
-  // //       credentials
-  // //     }
-  // //     const store = mockStore({})
-  // //
-  // //     store.dispatch(authenticateD2LStudent(credentials))
-  // //     .then( () => {
-  // //       store.getActions().should.be.eql(expectedAction)
-  // //     })
-  // //   })
-  // function deleteMissionAsync(missionId) {
-  //   return chai.request(BASE_URL)
-  //    .delete(`/middleman/banks/${ALGEBRA_BANK_ID}/missions/${missionId}`)
-  //    .then((res) => {
-  //     //  console.log('delete res', res.text)
-  //      return res.text ? JSON.parse(res.text) : null;
-  //    });
-  // }
+
+  //   it('should be able to get and respond to questions for a mission', done => {
+  //     const expectedAction = {
+  //       type: RECEIVE_CREATE_TAKE_MISSION,
+  //       credentials
+  //     }
+  //     const store = mockStore({})
   //
-  // function deleteAuthzAsync(student) {
-  //   return chai.request(BASE_URL)
-  //    .delete(`/middleman/banks/${ALGEBRA_BANK_ID}/missions/${missionId}`)
-  //    .then((res) => {
-  //     //  console.log('delete res', res.text)
-  //      return res.text ? JSON.parse(res.text) : null;
-  //    });
-  // }
-  //
-  // function cleanUpPromise(student) {
-  //   console.log('cleaning up for', student);
-  //
-  //   return chai.request(BASE_URL)
-  //   .get(`/middleman/banks/${ALGEBRA_BANK_ID}/missions`)
-  //   .set('x-fbw-username', student.agentId)
-  //   .then( (res) => {
-  //     res.should.have.status(200);
-  //
-  //     let result = JSON.parse(res.text);
-  //     let phaseIIs = _.filter(result, mission => mission.displayName.text.indexOf('Phase II') > -1 || mission.offereds.length == 0);
-  //     // console.log('all missions', result);
-  //     // console.log(result[0].displayName);
-  //
-  //     // console.log('missions to be deleted', _.map(phaseIIs, 'displayName.text'));
-  //
-  //     // need to delete the bank
-  //     // need to delete the bank hierarchy
-  //     return Q.all(_.map(phaseIIs, mission => deleteMissionAsync(mission.id)))
+  //     store.dispatch(selectOpenMission())
+  //     .then( () => {
+  //       // Submit to a question here???
+  //     })
   //   })
-  //   .then( res => res)
-  //   .catch( err => err);
-  // }
   //
-  // // clean up all the newly-created authorizations, banks, and missions
-  // after( function(done) {
-  //   this.timeout(20000);
-  //
-  //   Q.all(cleanUpPromise(UNIQUE_USERNAME))
-  //   .then( res => {
-  //     console.log('cleaned up for all newly created students', res.text);
-  //
-  //     done();
-  //   })
-  // })
+
+  function cleanUpPromise(student) {
+    console.log('cleaning up for', student);
+
+    return chai.request(BASE_URL)
+    .delete(`/middleman/banks/${privateBankId}`)
+    .then( (res) => {
+      res.should.have.status(200);
+      return chai.request(BASE_URL)
+      .delete(`/middleman/authorizations`)
+      .set('x-fbw-username', LOGGED_IN_USERNAME)
+    })
+    .then( (res) => {
+      res.should.have.status(200)
+      return Q.when('')
+    })
+    .catch( err => err);
+  }
+
+  // clean up all the newly-created authorizations, banks, and missions
+  after( function(done) {
+    this.timeout(20000);
+
+    cleanUpPromise(LOGGED_IN_USERNAME)
+    .then( res => {
+      console.log('cleaned up for all newly created students', res.text);
+
+      done();
+    })
+  })
 })
