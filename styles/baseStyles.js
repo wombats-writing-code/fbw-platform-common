7a9a03a34729f5ba1b74c8539c7dc09fec9b97d5
luwen-huang <huang.luwen@gmail.com>
2017-03-09 13:46:23 -0600

    publish now transpiles to es5 first

---
+++ styles/baseStyles.js
@@ -0,0 +1,13 @@
+Object.defineProperty(exports,"__esModule",{value:true});
+
+var BASE_STYLES={
+flexContainer:{
+display:'flex'},
+
+primaryColor:'#324D5C',
+primaryColorDark:'#46B29D',
+warningColor:'#FF6F69',
+linkColor:'#00A388'};exports['default']=
+
+
+BASE_STYLES;
