e1f17a2e055dc0373b4a3ea7ae80daf96a4453b4
luwen-huang <huang.luwen@gmail.com>
2017-03-09 13:38:54 -0600

    vb

---
+++ styles/base-styles.js
@@ -0,0 +1,23 @@
+
+const BASE_STYLES = {
+  appBlueDark: '#2C3E50',
+  appBlue2: '#2980B9',
+  appRed: '#E74C3C',
+  appOrange: '#E37B40',
+  appGreen: '#86bEa4',
+  loadingText: {
+
+  },
+  activityIndicator: {
+    marginTop: 32
+  },
+  textCenter: {
+    textAlign: 'center'
+  },
+  flexRow: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+};
+
+module.exports = BASE_STYLES;
