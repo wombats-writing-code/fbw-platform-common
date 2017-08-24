5fae02a7c13dbd232f43e29c69608eca789dcb91
cjshaw <4930129+cjshawMIT@users.noreply.github.com>
2017-08-24 12:15:49 -0400

    make sure alt-text is empty for the logo

--- src/components/nav-bar/__tests__/NavBar.spec.js
+++ src/components/nav-bar/__tests__/NavBar.spec.js
@@ -86,6 +86,7 @@ describe('NavBar', () => {
 
     component.find('.breadcrumb').length.should.eql(1);  // Home
     component.find('a').first().prop('aria-label').should.eql('Home');
+    component.find('img').first().prop('alt-text').should.eql('');
   });
 
   after(() => {
